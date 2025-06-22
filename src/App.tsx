28d9371ea3835cb48fd274ed4e3abf65aeeb4f42
willsf2021 <willsf2021@gmail.com>
2025-06-22 12:25:49 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetter, setGuessedLetter] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+  return <h1>Hi</h1>;
+}
+
+export default App;
